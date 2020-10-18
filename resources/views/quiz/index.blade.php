da82f38df77e1c4723848ec44423563bed312c36
audrey-134957 <milard.audrey@gmail.com>
2020-10-18 22:04:57 +0200

    intégrations ok

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -23,7 +23,7 @@
         @foreach($quizes as $quiz)
         <a class="quizes-list__link" href="{{route('quiz.startQuiz', ['quizId' => $quiz->id, 'slug' => $quiz->slug])}}">
             <div class="quizes-list__quiz-box quizes-list__quiz-box--geo uk-card uk-card-default uk-card-body" style="background-image:linear-gradient(rgba(0, 0, 0, 0.301), rgba(0, 0, 0, 0.301)), url('/images/boxes/{{$quiz->illustration}}') !important;">
-                <span class="quizes-list__questions-number">20</span>
+                <span class="quizes-list__questions-number">{{$quiz->questions->count()}}</span>
                 <span class="quizes-list__title">{{$quiz->title}}</span>
             </div>
         </a>
